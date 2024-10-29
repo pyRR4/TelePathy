6e3c9671ae5b82a5f93fa0da15483bf6d24a57ac
Igor <igopood33@gmail.com>
2024-10-29 19:01:56 +0100

    starting with project

--- app/src/main/java/com/example/telepathy/MainActivity.kt
+++ app/src/main/java/com/example/telepathy/MainActivity.kt
@@ -4,13 +4,25 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.telepathy.ui.theme.TelePathyTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,11 +31,30 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             TelePathyTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    Column (
+                        verticalArrangement = Arrangement.Top,
+                        modifier = Modifier
+                            .background(color = Color.DarkGray)
+                            .padding(top = 40.dp)
+                            .fillMaxWidth()
+                    ) {
+                        Header(
+                            text = stringResource(com.example.telepathy.R.string.your_contacts),
+                            modifier = Modifier.align(
+                                Alignment.CenterHorizontally
+                            )
+                        )
+
+                        Column (
+                            modifier = Modifier
+                        ) {
+
+                        }
+                    }
                 }
             }
         }
@@ -31,17 +62,41 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
+fun Header(text: String, modifier: Modifier = Modifier) {
     Text(
-        text = "Hello $name!",
-        modifier = modifier
+        text = text,
+        color = Color.White,
+        fontSize = 48.sp,
+        textAlign = TextAlign.Center
     )
 }
 
+@Composable
+fun Contact() {
+    Row(
+
+    ) {
+
+    }
+}
+
+
+@Composable
+fun Avatar() {
+
+}
+
+
+@Composable
+fun ContactText() {
+
+}
+
+
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
+fun TelePathyPreview() {
     TelePathyTheme {
-        Greeting("Android")
+        Contact()
     }
 }
