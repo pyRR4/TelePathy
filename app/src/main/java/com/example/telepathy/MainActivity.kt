3d9932c43b2a0ddaeb709d86a9def11551efe717
Igor <igopood33@gmail.com>
2024-11-11 18:16:36 +0100

    fixes after merging

--- app/src/main/java/com/example/telepathy/MainActivity.kt
+++ app/src/main/java/com/example/telepathy/MainActivity.kt
@@ -4,12 +4,16 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import com.example.telepathy.ui.screens.ContactCard
 import com.example.telepathy.ui.screens.MainScreen
 import com.example.telepathy.ui.screens.SettingsScreen
 import com.example.telepathy.ui.screens.ContactsScreen
+import com.example.telepathy.ui.theme.TelePathyTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -28,5 +32,24 @@ fun MyApp() {
         composable("main") { MainScreen(navController) }
         composable("settings") { SettingsScreen(navController) }
         composable("contacts") { ContactsScreen(navController) }
+    }
+}
+
+
+
+@Preview(showBackground = true)
+@Composable
+fun TelePathyPreview() {
+    TelePathyTheme {
+        //ContactsScreen()
+        ContactCard(
+            R.drawable.test,
+            "AmatorUczciwiec000",
+            true,
+            "siema",
+            "15:37",
+            Color.Blue,
+            { println("Button clicked!") }
+        )
     }
 }
--- app/src/main/java/com/example/telepathy/ui/screens/ContactsScreen.kt
+++ app/src/main/java/com/example/telepathy/ui/screens/ContactsScreen.kt
@@ -4,10 +4,8 @@ import android.util.Log
 import androidx.activity.compose.BackHandler
 import androidx.compose.foundation.background
 import androidx.compose.foundation.gestures.detectDragGestures
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Text
+import androidx.compose.material3.Text
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -16,6 +14,128 @@ import androidx.compose.ui.input.pointer.pointerInput
 import androidx.navigation.NavHostController
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.requiredSize
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Surface
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+
+
+@Composable
+fun Header(text: String, modifier: Modifier = Modifier) {
+    Text(
+        text = text,
+        color = Color.White,
+        fontSize = 48.sp,
+        textAlign = TextAlign.Center,
+        modifier = modifier
+    )
+}
+
+
+@Composable
+fun Avatar(image: Painter, modifier: Modifier) {
+
+    Image (
+        painter = image,
+        contentDescription = null,
+        contentScale = ContentScale.Crop,
+        modifier = modifier
+            .requiredSize(90.dp)
+            .clip(CircleShape)
+    )
+
+}
+
+@Composable
+fun ContactText(name: String, isFromUser: Boolean, message: String, time: String, modifier: Modifier) {
+    var msg = message;
+    if (isFromUser) {
+        msg = "Ty:\n$msg"
+    } else {
+        msg = name + '\n' + msg
+    }
+    Row (
+        modifier = modifier
+            .fillMaxWidth()
+            .fillMaxHeight()
+    ) {
+        Column (
+            modifier = Modifier.weight(1f)
+        ) {
+            Text(
+                text = name,
+                fontSize = 18.sp,
+                fontWeight = FontWeight.Bold,
+                color = Color.White
+            )
+
+            Text(
+                text = msg,
+                fontSize = 14.sp,
+                color = Color.White.copy(alpha = 0.8f),
+                lineHeight = 18.sp,
+                modifier = Modifier.padding(top = 10.dp)
+            )
+        }
+
+        Text (
+            text = time,
+            fontSize = 14.sp,
+            color = Color.White.copy(alpha = 0.8f),
+            textAlign = TextAlign.End
+        )
+    }
+}
+
+@Composable
+fun ContactCard(
+    imageDrawable: Int,
+    name: String,
+    isFromUser: Boolean,
+    message: String,
+    time: String,
+    backgroundColor: Color,
+    onClick: () -> Unit
+) {
+    val buttonModifier = Modifier
+        .size(height = 140.dp, width = 570.dp)
+        .clickable(onClick = onClick)
+        .padding(horizontal = 16.dp, vertical = 8.dp)
+        .background(color = backgroundColor, shape = RoundedCornerShape(20.dp))
+        .padding(16.dp)
+
+    Row (
+        modifier = buttonModifier,
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        val avatarModifier = Modifier
+            .align(Alignment.CenterVertically)
+
+        val textModifier = Modifier
+            .padding(horizontal = 8.dp, vertical = 8.dp)
+        Avatar(painterResource(imageDrawable), avatarModifier)
+        ContactText(name, isFromUser, message, time, textModifier)
+    }
+}
 
 @Composable
 fun ContactsScreen(navController: NavHostController) {
@@ -34,7 +154,7 @@ fun ContactsScreen(navController: NavHostController) {
         }
     }
 
-    Box(
+    Surface(
         modifier = Modifier
             .fillMaxSize()
             .background(Color.Magenta)
@@ -59,10 +179,26 @@ fun ContactsScreen(navController: NavHostController) {
                 }
             }
     ) {
-        Text(
-            text = "Contacts Screen",
-            modifier = Modifier.align(Alignment.Center),
-            style = MaterialTheme.typography.h4
-        )
+        Column (
+            verticalArrangement = Arrangement.Top,
+            modifier = Modifier
+                .background(color = Color.DarkGray)
+                .padding(top = 40.dp)
+                .fillMaxWidth()
+        ) {
+            Header(
+                text = stringResource(com.example.telepathy.R.string.your_contacts),
+                modifier = Modifier
+                    .align(
+                        Alignment.CenterHorizontally
+                    )
+            )
+
+            Column (
+                modifier = Modifier
+            ) {
+
+            }
+        }
     }
-}
+    }
