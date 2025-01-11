8fda9382a09d5b798823c6fc80658187395d7e5b
Igor <igopood33@gmail.com>
2025-01-11 21:18:40 +0100

    created PermissionsManager.kt for #22

--- app/src/main/java/com/example/telepathy/MainActivity.kt
+++ app/src/main/java/com/example/telepathy/MainActivity.kt
@@ -1,10 +1,7 @@
 package com.example.telepathy
 
-import android.Manifest
-import android.content.pm.PackageManager
 import android.os.Build
 import android.os.Bundle
-import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.annotation.RequiresApi
@@ -16,24 +13,22 @@ import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.core.app.ActivityCompat
-import androidx.core.content.ContextCompat
-import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.compose.rememberNavController
 import com.example.telepathy.data.AppDatabase
 import com.example.telepathy.data.PreferencesManager
+import com.example.telepathy.domain.PermissionManager
 import com.example.telepathy.presentation.navigation.AnimatedNavHost
 import com.example.telepathy.presentation.ui.theme.TelePathyTheme
-import com.example.telepathy.presentation.viewmodels.GenericViewModelFactory
-import com.example.telepathy.presentation.viewmodels.SharedViewModel
 
-private const val REQUEST_CODE_BLUETOOTH = 101
-
+@RequiresApi(Build.VERSION_CODES.UPSIDE_DOWN_CAKE)
 class MainActivity : ComponentActivity() {
-    @RequiresApi(Build.VERSION_CODES.UPSIDE_DOWN_CAKE)
+
+    private lateinit var permissionManager: PermissionManager
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        getBluetoothPermissions(this)
+        permissionManager = PermissionManager(this)
+        permissionManager.checkAndRequestBluetoothPermissions()
 
         val database = AppDatabase.getDatabase(applicationContext)
 
@@ -48,44 +43,7 @@ class MainActivity : ComponentActivity() {
         grantResults: IntArray
     ) {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults)
-        if (requestCode == REQUEST_CODE_BLUETOOTH) {
-            permissions.forEachIndexed { index, permission ->
-                val result = if (grantResults[index] == PackageManager.PERMISSION_GRANTED) "granted" else "denied"
-                Log.d("BluetoothPermissions", "Permission: $permission -> $result")
-            }
-        }
-    }
-
-
-    @RequiresApi(Build.VERSION_CODES.S)
-    private fun getBluetoothPermissions(activity: ComponentActivity) {
-        val requiredPermissions = mutableListOf<String>()
-
-        if (ContextCompat.checkSelfPermission(activity, Manifest.permission.BLUETOOTH_SCAN) != PackageManager.PERMISSION_GRANTED) {
-            requiredPermissions.add(Manifest.permission.BLUETOOTH_SCAN)
-        }
-        if (ContextCompat.checkSelfPermission(activity, Manifest.permission.BLUETOOTH_CONNECT) != PackageManager.PERMISSION_GRANTED) {
-            requiredPermissions.add(Manifest.permission.BLUETOOTH_CONNECT)
-        }
-        if (ContextCompat.checkSelfPermission(activity, Manifest.permission.BLUETOOTH_ADVERTISE) != PackageManager.PERMISSION_GRANTED) {
-            requiredPermissions.add(Manifest.permission.BLUETOOTH_ADVERTISE)
-        }
-        if (ContextCompat.checkSelfPermission(activity, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-            requiredPermissions.add(Manifest.permission.ACCESS_COARSE_LOCATION)
-        }
-        if (ContextCompat.checkSelfPermission(activity, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-            requiredPermissions.add(Manifest.permission.ACCESS_FINE_LOCATION)
-        }
-
-        if (requiredPermissions.isNotEmpty()) {
-            ActivityCompat.requestPermissions(
-                activity,
-                requiredPermissions.toTypedArray(),
-                REQUEST_CODE_BLUETOOTH
-            )
-        } else {
-            Log.d("BluetoothPermissions", "All required permissions already granted.")
-        }
+        permissionManager.handlePermissionsResult(requestCode, permissions, grantResults)
     }
 }
 
